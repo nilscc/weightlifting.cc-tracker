7ecc7e92ee75064be02c43986068dfcc509f87e7
Nils Schweinsberg <mail@nils.cc>
2020-02-17 00:58:45 +0100

    Add time and date picker to new workout page

--- lib/widgets/add_workout.dart
+++ lib/widgets/add_workout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AddWorkoutPage extends StatefulWidget {
   AddWorkoutPage({Key key}) : super(key: key);
@@ -10,6 +11,10 @@ class AddWorkoutPage extends StatefulWidget {
 }
 
 class _AddWorkoutPageState extends State<AddWorkoutPage> {
+
+  DateTime _date = DateTime.now();
+  TimeOfDay _time = null;
+
   void save() {}
 
   AppBar buildAppBar() {
@@ -24,49 +29,62 @@ class _AddWorkoutPageState extends State<AddWorkoutPage> {
     );
   }
 
-  Future<Widget> buildBody(BuildContext context) async {
+  String formattedDateTime() {
+    if (_time == null) {
+      return DateFormat.yMd().format(_date);
+    }
+    else {
+      return DateFormat.yMd().add_jm().format(DateTime(_date.year, _date.month, _date.day, _time.hour, _time.minute));
+    }
+  }
 
-    final DateTime date = await showDatePicker(
-        context: context,
-        initialDate: DateTime.now(),
-        firstDate: DateTime.now().subtract(Duration(days: 365)),
-        lastDate: DateTime.now());
+  Widget buildAddTime(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.access_time),
+      onPressed: () async {
+        final TimeOfDay newTime = await showTimePicker(
+            context: context,
+            initialTime: _time != null ? _time : TimeOfDay.now());
 
-    return ListView(
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Container(
-              width: 50,
-              child: Text('Date:'),
-            ),
-            Container(
-              child: Text('${date.day}.${date.month}.${date.year}'),
-            ),
-          ],
-        )
-      ],
+        setState(() {
+          _time = newTime;
+        });
+      },
+    );
+  }
+
+  Widget buildBody(BuildContext context) {
+    return Card(
+      child: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.calendar_today),
+            trailing: buildAddTime(context),
+            onTap: () async {
+              final DateTime newDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime.now());
+
+              if (newDate != null) {
+                setState(() {
+                  _date = newDate;
+                });
+              }
+            },
+            title: Text('${formattedDateTime()}'),
+          ),
+        ],
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: buildAppBar(),
-      body: FutureBuilder<Widget>(
-        future: buildBody(context),
-        builder: (BuildContext context, AsyncSnapshot<Widget> snapshot) {
-          if (snapshot.hasData) {
-            return snapshot.data;
-          }
-          else {
-            return Center(
-                child: CircularProgressIndicator(),
-            );
-          }
-        }
-      ),
+      body: buildBody(context),
     );
   }
 }
