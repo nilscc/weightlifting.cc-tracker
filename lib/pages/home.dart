fda11253cc7bc471ef211ca44778e0fa40a8183b
nils.cc <mail@nils.cc>
2020-09-14 02:50:24 +0200

    Use ListView to make workout list scrollable

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -37,7 +37,7 @@ class HomePage extends StatelessWidget {
    *
    */
 
-  Widget _body(BuildContext context) => Column(
+  Widget _body(BuildContext context) => ListView(
         children: <Widget>[
           _header(context),
           FutureBuilder(
